002ddddba58ee623c9d21df04526a64384afc8f7
DasunNethsara-04 <techsaralk.pro@gmail.com>
2025-07-10 13:21:11 +0530

    Add program1.c to implement basic character and string output in C

---
+++ C/day4/unformatted_outputs/program1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    // putchar() - to output a character
+    putchar('c');
+    putchar('\n');
+
+    // puts() - to output a complete string
+    puts("C is a hell!");
+    putchar('\n');
+    return 0;
+}

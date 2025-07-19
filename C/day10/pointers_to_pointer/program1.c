d03b33e236fd2e318f5a6902daef550de9fb37ed
DasunNethsara-04 <techsaralk.pro@gmail.com>
2025-07-19 15:16:46 +0530

    files restructured

--- C/day10/pointers_to_pointer/program1.c
+++ C/day10/pointers_to_pointer/program1.c
@@ -2,6 +2,18 @@
 
 int main()
 {
+    int a = 10;
+
+    int *p = &a;
+    int **q = &p;
+    int ***r = &q;
+
+    printf("%d %d %d %d\n", a, *p, **q, ***r);
+}
+#include <stdio.h>
+
+int main()
+{
     int a[5] = {2, 6, 1, 8, 9};
 
     int *ptr = &a[0];

5505e0d2c0c43223e095cb548756f5b96b4a86db
DasunNethsara-04 <techsaralk.pro@gmail.com>
2025-07-23 19:37:04 +0530

    feat: implement number guessing game

---
+++ C/projects/beginner/1-number_guessing_game/app.c
@@ -0,0 +1,75 @@
+//
+// Created by Dasun Nethsara on 7/23/25.
+//
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int generate_random_number(const int range)
+{
+    srand(time(NULL));
+    const int randomNumber = rand() % range;
+    return randomNumber;
+}
+
+void play_game(const int range)
+{
+    int tries = 0;
+    int input;
+    const int randomNum = generate_random_number(range);
+
+    // get user input
+    while (1)
+    {
+        tries++;
+        printf("Enter Your Guess: ");
+        scanf("%d", &input);
+
+        if (input == randomNum)
+        {
+            printf("You Won!\n");
+            printf("Tries: %d\n", tries);
+            break;
+        }
+
+        if (input > randomNum)
+        {
+            printf("Your guess is grater than the actual number!\n");
+        }
+        else if (input < randomNum)
+        {
+            printf("Your guess is less than the actual number!\n");
+        }
+        else
+        {
+            printf("Invalid Input1\n");
+        }
+    }
+}
+
+int main(void)
+{
+    int difficulty = 0;
+
+    printf("Choose Difficulty (\n\t1 - Easy\n\t2 - Medium\n\t3 - Hard): ");
+    scanf("%d", &difficulty);
+
+    switch (difficulty)
+    {
+    case 1:
+        play_game(100);
+        break;
+    case 2:
+        play_game(1000);
+        break;
+    case 3:
+        play_game(10000);
+        break;
+    default:
+        printf("Invalid Argument!\n");
+        break;
+    }
+
+    return 0;
+}

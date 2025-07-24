e3b1e1bd2048c793a023baeeb702ef482327de4b
DasunNethsara-04 <techsaralk.pro@gmail.com>
2025-07-24 23:56:45 +0530

    feat: add Palindrome Checker project with makefile

---
+++ C/projects/beginner/5-palindrome_check/app.c
@@ -0,0 +1,37 @@
+//
+// Project 4 - Palindrome Checker (string)
+// Created by Dasun Nethsara on 7/24/25.
+//
+
+#include <stdio.h>
+#include <string.h>
+
+int is_palindrome(char string[])
+{
+    int flag = 1;
+    for (int i = 0, len = strlen(string); i < len / 2; i++)
+    {
+        if (string[i] != string[len - 1 - i])
+        {
+            flag = 0;
+            break;
+        }
+    }
+    return flag;
+}
+
+int main()
+{
+    char string[30];
+
+    // get the user input
+    printf("Enter Text: ");
+    scanf("%s", string);
+
+    if (is_palindrome(string))
+        printf("It is palindrome!\n");
+    else
+        printf("It is not a palindrome!\n");
+
+    return 0;
+}

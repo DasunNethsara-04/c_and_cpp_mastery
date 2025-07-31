1fa6165cae1f0634e93d757aa45167b1f24e39d1
DasunNethsara-04 <techsaralk.pro@gmail.com>
2025-07-31 15:26:37 +0530

    edit user function implemented

--- C/projects/intermediate/4-library_management_system/utils/user.c
+++ C/projects/intermediate/4-library_management_system/utils/user.c
@@ -133,7 +133,69 @@ int search_user(int role)
 
 int edit_user()
 {
-    //
+    FILE *fptr1;
+    FILE *fptr2;
+    Line line;
+    struct User users[MAX_USERS];
+    char query[MAX_USER_NAME_LEN];
+    int users_len = get_length(USERS_DB);
+
+    char temp_name[MAX_USER_NAME_LEN];
+
+    fptr1 = fopen(USERS_DB, "r");
+    if (fptr1 == NULL)
+    {
+        printf("Unable to load the users database!\n");
+        return 1;
+    }
+
+    printf("\nEdit book\n");
+
+    printf("User Name: ");
+    fgets(query, MAX_USER_NAME_LEN, stdin);
+    size_t len1 = strlen(query);
+    if (len1 > 0 && query[len1 - 1] == '\n')
+        query[len1 - 1] = '\0';
+
+    for (int i = 0; i < users_len; i++)
+    {
+        if (fgets(&line, sizeof(line), fptr1) != NULL)
+        {
+            sscanf(&line, "%[^\n|]|", users[i].name);
+            if (strcmp(query, users[i].name) == 0)
+            {
+                printf("Enter Name: ");
+                fgets(temp_name, sizeof(temp_name), stdin);
+                size_t len2 = strlen(temp_name);
+                if (len2 > 0 && query[len2 - 1] == '\n')
+                    temp_name[len2 - 1] = '\0';
+
+                if (strcmp(temp_name, users[i].name) != 0)
+                {
+                    strcpy(users[i].name, temp_name);
+                }
+            }
+        }
+    }
+
+    fclose(fptr1);
+
+    // TODO: fix the bug
+    fptr2 = fopen(USERS_DB, "w");
+    if (fptr2 == NULL)
+    {
+        printf("Unable to load the books database!\n");
+        return 1;
+    }
+
+    // write the content in the book[] into the file
+    for (int i = 0; i < users_len; i++)
+    {
+        fprintf(fptr2, "%s|\n", users[i].name);
+    }
+    printf("User updated!\n");
+    fclose(fptr2);
+    return 0;
 }
 
 int delete_user(int role)
